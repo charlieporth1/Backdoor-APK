03f7c7dc60bf1ea8dd6dd2df95f99425dabfac0d
Charles Porth <charlieporth1@gmail.com>
2016-11-29 18:53:28 -0600

    Add files via upload

---
+++ msfvenom-backdoor-android-master/app/src/main/gen/stage/metasploit/com/backdooredapk/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package stage.metasploit.com.backdooredapk;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
